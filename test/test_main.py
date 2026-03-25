cbd01c73315a53b457fd4953509cab9a0f388302
Rodrigues Silva Filho <Eduardo.RodriguesSilvaFilho@dlr.de>
2026-03-25 14:33:47 +0100

    added distance function test

--- test/test_main.py
+++ test/test_main.py
@@ -1,2 +1,10 @@
+from vicsek.vicsek_bad import VicsekModel
+import numpy as np
+
+def test_distance():
+  r1 = np.array([1,1])
+  r2 = np.array([2,2])
+  assert(VicsekModel.distance(r1, r2) == np.sqrt(2))
+  
 def test_main():
   assert(True)
