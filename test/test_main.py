938c6f2621be09e65c017d4cec13f83abebb28ce
ClaraVth <clara.vaeth@web.de>
2026-03-25 14:59:36 +0100

    test animation creation

--- test/test_main.py
+++ test/test_main.py
@@ -1,2 +1,17 @@
+from vicsek.vicsek_bad import VicsekModel
+from matplotlib.animation import FuncAnimation
+import os
+
 def test_main():
   assert(True)
+
+def test_figure():
+  vicsek_model = VicsekModel()
+  vicsek_model.init_model(200)
+  ani = FuncAnimation(vicsek_model.fig, vicsek_model.animate, frames=20, interval=50, blit=True)
+  outputfile = "animation.gif"
+  #output_path.parent.mkdir(parents=True, exist_ok=True)
+  ani.save(outputfile, writer="pillow")
+
+  assert(os.path.exists(outputfile))
+  #assert(outputfile.stat().st_size > 10)
